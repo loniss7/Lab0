79df8c86a51a20b387638149c839241c23d1b97f
loniss7 <leonkrup700@gmail.com>
2025-09-25 14:40:19 +0300

    updt

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-alert("Let's integrate!")
+alert("Lab0");
+
+function f1(x) { return x ** 2 - Math.cos(x); }
+
+function leftRiemann(f, a, b, n) {
+    let h = (b - a) / n, sum = 0;
+    for (let i = 0; i < n; i++) sum += f(a + i * h);
+    return sum * h;
+}
+
+let a = parseFloat(prompt("Введите начало интервала (a):"));
+let b = parseFloat(prompt("Введите конец интервала (b):"));
+let n = 10;
+let result = leftRiemann(f1, a, b, n);
+
+alert("Приближённое значение интеграла: F(x) = x ** 2 - cos(x) на [" + a + ", " + b + "] равно " + result.toFixed(6));
